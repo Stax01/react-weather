9d78c615e5af301be2dd5ea53f6147acab7fd3be
Vladimir <83559027+Stax01@users.noreply.github.com>
2021-11-25 10:51:13 +0300

    Add files via upload

---
+++ DaysCard.tsx
@@ -0,0 +1,30 @@
+
+import * as _ from 'lodash'
+import React from 'react'
+import { GlobalSvgSelector } from '../../../../assets/icon/global/globalSvgSelector'
+import Popup from '../../../../components/popup/Popup'
+import { WeatherDaily } from '../../../../store/types'
+import s from './days.module.scss'
+
+
+const DaysCard = (data: WeatherDaily) => {
+    const main = _.lowerCase(data.weather[0].main)
+    const [modal, setModal] = React.useState(false)
+
+    return (
+        <div
+            className={s.card}
+            onClick={() => setModal(!modal)}
+        >
+            <Popup modal={modal} data={data} main={main} />
+            <div className={s.name}>{data.day}</div>
+            <div className={s.date}>{data.date} {data.montn}</div>
+            <GlobalSvgSelector id={main} />
+            <div className={s.temp}>{Math.floor(data.temp.day)}° Днем</div>
+            <div className={s.temp_2}>{Math.floor(data.temp.night)}° Ночью</div>
+            <div className={s.weather}>{data.weather[0].description}</div>
+        </div>
+    )
+}
+
+export default DaysCard
